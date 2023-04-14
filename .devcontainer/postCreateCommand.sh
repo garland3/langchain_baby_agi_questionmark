026c3610cb671db3d28575108b47e0ac833503c3
Anthony <garland3@gmail.com>
2023-04-14 01:45:46 +0000

    added some stuff

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,5 @@
+# copy github ssh key. 
+# if the dir exists, that is OK. make parents if needed. 
+echo "hello. post create command running"
+# mkdir -p /root/.ssh
+# cp ~/.ssh/id_rsa /root/.ssh/id_rsa
